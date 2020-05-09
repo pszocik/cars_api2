8a5bfaa34dae8aed207b7c1afd7e3798b3180bd6
mabbyn <Piotrek.szocik@gmail.com>
2020-05-09 09:59:03 +0200

    changed reservation validate to be more specific

--- api/serializers.py
+++ api/serializers.py
@@ -14,3 +14,17 @@ class ReservationSerializer(serializers.ModelSerializer):
         model = models.Reservation
         fields = ('id', 'car', 'reserved_by',
                   'reserved_from', 'reserved_to')
+
+    def validate(self, data):
+        if models.Reservation.objects.filter(
+                reserved_from__range=(data['reserved_from'], data['reserved_to'])) or models.Reservation.objects.filter(
+                reserved_to__range=(data['reserved_from'], data['reserved_to'])).filter(car=data['car']):
+            raise serializers.ValidationError(
+                "There are already some reservations at given date.")
+
+        if data['reserved_from'] > data['reserved_to']:
+            raise serializers.ValidationError("Finish must occur after start.")
+        if data['reserved_to'] > models.Car.objects.get(name=data['car']).service_date:
+            raise serializers.ValidationError(
+                "You can't make a reservation after service date.")
+        return data
