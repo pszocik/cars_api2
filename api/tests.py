ef28e8813c5158459144471e6829f9035d82ee7a
mabbyn <Piotrek.szocik@gmail.com>
2020-05-10 18:14:16 +0200

    car model tests added

--- api/tests.py
+++ api/tests.py
@@ -10,30 +10,36 @@ from . import serializers
 
 
 class CarTestCase(APITestCase):
-    def test_api_get(self):
-        response = self.client.get('/api/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_cars_get(self):
+    def test_get_cars(self):
         response = self.client.get('/api/cars/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_car_create(self):
-        data = {
-            "name": "S40",
-            "brand": "Volvo",
-            "registration_number": "CXZ11AS",
-            "service_date": "2020-07-22T19:05:00Z"
-        }
-        response = self.client.post('/api/cars/', data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(models.Car.objects.count(), 1)
-        self.assertEqual(models.Car.objects.get().name, 'S40')
+    def setUp(self):
+        models.Car.objects.create(name='S40', brand='Volvo',
+                                  registration_number='CXZ11AS', service_date="2020-07-22T19:05:00Z")
 
-    def test_cars_get(self):
+    def test_get_car_info(self):
         response = self.client.get('/api/cars/1/')
-        print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_car(self):
+        response = self.client.get('/api/cars/9999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_put_car(self):
+        data = {
+            "name": "Punto",
+            "brand": "Fiat",
+            "registration_number": "NOL13S1",
+            "service_date": "2020-07-22T15:05:00Z",
+        }
+        response = self.client.put('/api/cars/1/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_car(self):
+        response = self.client.delete('/api/cars/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.Car.objects.count(), 0)
 
 # class ReservationTestCase(APITestCase):
 
